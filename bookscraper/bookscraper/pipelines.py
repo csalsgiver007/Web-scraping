84749d7df42a3a6feebd2ca50b93f2c1104dff1d
Charles Salsgiver <none@gmail.com>
2025-02-05 15:43:59 -0500

    initial commit

---
+++ bookscraper/bookscraper/pipelines.py
@@ -0,0 +1,28 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class BookscraperPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        ## Strip all whitespace
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+        #Category and Product Type --> Switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()         
+
+        return item
